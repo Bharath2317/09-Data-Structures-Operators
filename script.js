f2e12011171d7ecc5d2878648ebc4da4403baffd
bharath <bharathh1817@gmail.com>
2025-09-12 22:33:06 +0530

    completed challenge upto now

--- script.js
+++ script.js
@@ -68,40 +68,40 @@ const restaurant = {
   },
 };
 
-///// *****The Nullish coalescing Operator (??)********* //////
+// ///// *****The Nullish coalescing Operator (??)********* //////
 
-// This operator consider both the  "0" and " "///
+// // This operator consider both the  "0" and " "///
 
-console.log(0 ?? 'jonas');
-console.log('' ?? 'jonas');
+// console.log(0 ?? 'jonas');
+// console.log('' ?? 'jonas');
 
-restaurant.num = 0;
-const guest94 = restaurant.num ?? 'jonas';
-console.log(guest94);
+// restaurant.num = 0;
+// const guest94 = restaurant.num ?? 'jonas';
+// console.log(guest94);
 
-//// ****Short Circuiting(&& and ||)****** ////
-console.log('' || 'jonas');
-console.log(null || 0); //print the second falsy value
-console.log(23 || 'johnas'); //gives the first truthy value
-console.log(0 || '' || 'john'); // ignores the falsy value due to short circuiting
+// //// ****Short Circuiting(&& and ||)****** ////
+// console.log('' || 'jonas');
+// console.log(null || 0); //print the second falsy value
+// console.log(23 || 'johnas'); //gives the first truthy value
+// console.log(0 || '' || 'john'); // ignores the falsy value due to short circuiting
 
-restaurant.num = 23;
-const guest = restaurant.num ? restaurant.num : 'no';
-console.log(guest); //since restaurant.num is true it has given value otherwise it may become false
+// restaurant.num = 23;
+// const guest = restaurant.num ? restaurant.num : 'no';
+// console.log(guest); //since restaurant.num is true it has given value otherwise it may become false
 
-const guest1 = restaurant.num || 10;
-console.log(guest1);
+// const guest1 = restaurant.num || 10;
+// console.log(guest1);
 
-console.log('____&&_____');
-//it is exact reverse of "||"
+// console.log('____&&_____');
+// //it is exact reverse of "||"
 
-console.log('' && 'jonas');
-console.log(null && 0);
-console.log(23 && 'johnas');
-console.log(0 && '' && 'john');
+// console.log('' && 'jonas');
+// console.log(null && 0);
+// console.log(23 && 'johnas');
+// console.log(0 && '' && 'john');
 
-const guest12 = restaurant.num && 10;
-console.log(guest12);
+// const guest12 = restaurant.num && 10;
+// console.log(guest12);
 
 // // ***REST OPERATOR***//
 // // It is spread if it is in the right sife of the "="
@@ -250,3 +250,84 @@ console.log(guest12);
 // const array4 = [3, 4];
 // const [i = 2, j = 1, m = 1, k = 1, l = 1] = array4;
 // console.log(i, j, m, k, l);
+
+// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...teamplayers] = players1;
+console.log(gk, teamplayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const plyersfinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(plyersfinal);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printgoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals has been scored`);
+};
+
+printgoals(...game.scored);
+game.scored.forEach(goal => console.log(goal));
+
+team1 < team2 && console.log('team1 is more likely to win');
+team1 > team2 && console.log('team2 is likely to win');
